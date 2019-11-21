21446f3e400907f1e21d7cf07d2780dc25380560
Yavanna Perez <yavannaperez@hotmail.com>
2019-11-21 15:35:14 +0100

    test save une version 'normale'

---
+++ SoundDesignLabTest/Assets/Sci-Fi Styled Modular Pack/Scripts/OpenTheDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTheDoor : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    AudioSource sound;
+
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        myAnimationController.SetBool("character_nearby", true);
+        sound.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        myAnimationController.SetBool("character_nearby", false);
+        sound.Stop();
+    }
+}
